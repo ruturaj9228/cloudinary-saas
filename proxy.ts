44dc6b1fdda43918922dd65389ff928ce81d528c
ruturaj9228 <ruturaj9228@gmail.com>
2026-06-21 20:15:51 +0530

    Add Clerk authentication middleware and update dependencies

---
+++ proxy.ts
@@ -0,0 +1,44 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse} from 'next/server'
+
+const isPublicRoute = createRouteMatcher([
+    '/sign-in',
+    '/sign-up',
+    '/home',
+    '/'
+])
+
+const isPublicApiRoute = createRouteMatcher([
+    '/api/videos',
+])
+
+
+export default clerkMiddleware(async (auth, req) => {
+    const {userId} = await auth()
+    const currentUrl = new URL(req.url)
+    const isaccessingDashboard = currentUrl.password === '/home'
+    const isApiRequest = currentUrl.pathname.startsWith('/api')
+
+    if(userId && isPublicRoute(req) && !isaccessingDashboard) {
+        return NextResponse.redirect(new URL('/home', req.url))
+    }
+
+    if(!userId && !isPublicRoute(req) && !isPublicApiRoute(req)) {
+        return NextResponse.redirect(new URL('/sign-in', req.url))
+    }
+
+    if(!userId && isApiRequest && !isPublicApiRoute(req)) {
+        return NextResponse.redirect(new URL('/sign-in', req.url))
+    }
+})
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+    // Always run for Clerk-specific frontend API routes
+    '/__clerk/(.*)',
+  ],
+}
